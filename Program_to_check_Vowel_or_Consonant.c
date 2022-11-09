5d16cb939b074b3a711a4876a071a8f39b42868d
Nagendra594 <111426323+Nagendra594@users.noreply.github.com>
2022-11-09 10:14:15 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     string s;     cin>>s;     if (s=="a" or s=="e" or s=="i" or s=="o" or s=="u" or  s=="A" or s=="E" or s=="I" or s=="O" or  s=="U"){         cout<<"Vowel";     }     else cout<<"Consonant"; }

---
+++ Program_to_check_Vowel_or_Consonant.c
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    if (s=="a" or s=="e" or s=="i" or s=="o" or s=="u" or  s=="A" or s=="E" or s=="I" or s=="O" or  s=="U"){
+        cout<<"Vowel";
+    }
+    else cout<<"Consonant";
+}
